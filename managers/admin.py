0c5811ee3e552ed06b90b625a793571d2139d37e
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-08 18:00:28 +0530

    updated

--- managers/admin.py
+++ managers/admin.py
@@ -1,3 +1,10 @@
+from os import name
 from django.contrib import admin
-
+from .models import Category, Medicine
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
