a7de84aab3473378cf1f7227bddb154dd6d5f939
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-09 23:42:44 +0530

    updated

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "username", "first_name", "last_name", 
+                    "is_customer", "is_manager", "is_staff", "is_active")
+    list_filter = ("is_customer", "is_manager", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("username", "first_name", "last_name")}),
+        (_("Roles"), {"fields": ("is_customer", "is_manager")}),
+        (
+            _("Permissions"),
+            {"fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )},
+        ),
+        (_("Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+
+    search_fields = ("email", "username")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True)
+    
+    first_name = models.CharField(max_length=120, blank=True)
+    last_name = models.CharField(max_length=120, blank=True)
+    
+    is_customer = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # no extra field when creating superuser
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
