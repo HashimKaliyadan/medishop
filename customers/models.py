4e3d2de6367a9f26e678a76190709abf35765003
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-09 21:48:16 +0530

    updated

--- customers/models.py
+++ customers/models.py
@@ -18,3 +18,41 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.line1}, {self.city} - {self.pincode}"
     
+class Cart(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="carts",
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.pk} for {self.user}"
+    
+    @property
+    def total(self):
+        return sum(item.line_total for item in self.items.all())
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete = models.CASCADE,
+        related_name="items"
+    )
+    medicine = models.ForeignKey(
+        Medicine,
+        on_delete = models.CASCADE,
+        related_name = "cart_items"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    class Meta:
+        unique_together = ("cart", "medicine")
+
+    def __str__(self):
+        return f"{self.medicine.name} x {self.quantity}"
+
+    @property
+    def line_total(self):
+        return self.medicine.price * self.quantity
