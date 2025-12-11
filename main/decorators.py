128af4a2d9d3d72b421a5b70489b507b255e9e15
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-11 19:40:34 +0530

    updated

--- main/decorators.py
+++ main/decorators.py
@@ -2,3 +2,13 @@
 from functools import wraps
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+
+def allow_manager(view_func):
+    @wraps(view_func)
+    @login_required
+    def _wrapped(request, *args, **kwargs):
+        user = request.user
+        if not getattr(user, "is_manager", False):
+            return redirect("/")   # change to any page you want for unauthorized
+        return view_func(request, *args, **kwargs)
+    return _wrapped
