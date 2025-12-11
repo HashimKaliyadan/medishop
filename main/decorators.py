c6cc8b25e2133773b79e73f78150de382b7bba8c
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-11 19:41:10 +0530

    updated

--- main/decorators.py
+++ main/decorators.py
@@ -12,3 +12,13 @@ def allow_manager(view_func):
             return redirect("/")   # change to any page you want for unauthorized
         return view_func(request, *args, **kwargs)
     return _wrapped
+
+def allow_customer(view_func):
+    @wraps(view_func)
+    @login_required
+    def _wrapped(request, *args, **kwargs):
+        user = request.user
+        if not getattr(user, "is_customer", False):
+            return redirect("/")   # change to customer landing if you want
+        return view_func(request, *args, **kwargs)
+    return _wrapped
