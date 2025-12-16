646f7ca3cd335eebc83f39d1aa4f8c1c0532e993
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-17 00:26:03 +0530

    updated

--- customers/urls.py
+++ customers/urls.py
@@ -5,5 +5,8 @@ app_name = "customers"
 
 urlpatterns = [
     path("", views.customer_home, name="home"),
-    path('account/', views.account, name="account"),
+    path("categories/", views.category_list, name="category-list"),
+    path("categories/<slug:slug>/", views.medicine_list_by_category, name="medicine-by-category"),
+    path("medicines/", views.all_medicines, name="medicine-list"),
+    path("medicine/<slug:slug>/", views.medicine_detail, name="medicine-detail"),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from main.decorators import allow_customer
+from django.shortcuts import render, get_object_or_404
+from managers.models import Category, Medicine
 
 def customer_home(request):
     return render(request, "customers/home.html")
 
-@allow_customer
-def account(request):
-    return render(request, "customers/account.html")
+def category_list(request):
+    categories = Category.objects.filter(is_active=True).order_by("name")
+    return render(request, "customers/category_list.html", {
+        "categories": categories
+    })
+
+def medicine_list_by_category(request, slug):
+    category = get_object_or_404(Category, slug=slug, is_active=True)
+    medicines = Medicine.objects.filter(
+        category=category,
+        in_stock=True
+    ).order_by("name")
+
+    return render(request, "customers/medicine_list.html", {
+        "category": category,
+        "medicines": medicines,
+    })
+
+def all_medicines(request):
+    medicines = Medicine.objects.filter(in_stock=True).order_by("name")
+    return render(request, "customers/all_medicines.html", {
+        "medicines": medicines
+    })
+
+def medicine_detail(request, slug):
+    medicine = get_object_or_404(Medicine, slug=slug, in_stock=True)
+    return render(request, "customers/medicine_detail.html", {
+        "medicine": medicine
+    })
