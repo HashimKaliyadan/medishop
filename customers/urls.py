252b61f4d86f3666b09d148e8ba54228cc54dc45
HashimKaliyadan <mohammadhashim9822@gmail.com>
2025-12-18 16:05:39 +0530

    updated

--- customers/urls.py
+++ customers/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path("categories/<slug:slug>/", views.medicine_list_by_category, name="medicine-by-category"),
     path("medicines/", views.all_medicines, name="medicine-list"),
     path("medicine/<slug:slug>/", views.medicine_detail, name="medicine-detail"),
+    path("cart/", views.cart_view, name="cart"),
+    path("cart/add/<int:medicine_id>/", views.add_to_cart, name="add-to-cart"),
+    path("cart/update/<int:item_id>/", views.update_cart, name="update-cart"),
+    path("cart/remove/<int:item_id>/", views.remove_from_cart, name="remove-from-cart"),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from managers.models import Category, Medicine
+from .models import Cart, CartItem
+from django.contrib.auth.decorators import login_required
 
 def customer_home(request):
     return render(request, "customers/home.html")
@@ -33,3 +35,45 @@ def medicine_detail(request, slug):
     return render(request, "customers/medicine_detail.html", {
         "medicine": medicine
     })
+
+@login_required
+def add_to_cart(request, medicine_id):
+    medicine = get_object_or_404(Medicine, id=medicine_id, in_stock=True)
+
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        medicine=medicine
+    )
+
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+
+    return redirect("customers:cart")
+
+@login_required
+def cart_view(request):
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+    return render(request, "customers/cart.html", {"cart": cart})
+
+@login_required
+def update_cart(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+
+    if request.method == "POST":
+        qty = int(request.POST.get("quantity", 1))
+        if qty > 0:
+            item.quantity = qty
+            item.save()
+        else:
+            item.delete()
+
+    return redirect("customers:cart")
+
+@login_required
+def remove_from_cart(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+    item.delete()
+    return redirect("customers:cart")
